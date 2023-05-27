375366a3130a972c9a08f9c4789232962e2321c4
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2023-05-27 11:55:26 +0700

    Render old blog (#1)

--- apps/www/app/blog/[id]/page.tsx
+++ apps/www/app/blog/[id]/page.tsx
@@ -1,14 +1,25 @@
-import type { Root } from "mdast";
+import type { Root as MdastRoot } from "mdast";
+import type { Metadata } from "next";
+import type { ComponentPropsWithoutRef } from "react";
+import type { Node as UnistNode } from "unist";
 
 import type { SC } from "~/server";
 
+import { clsx } from "clsx";
 import fs from "fs";
+import { toString as mdastToString } from "mdast-util-to-string";
+import Link from "next/link";
 import path from "path";
+import { forwardRef } from "react";
 import ReactMarkdown from "react-markdown";
+import rehypeRaw from "rehype-raw";
 import remarkFrontmatter from "remark-frontmatter";
+import remarkGfm from "remark-gfm";
 import remarkParse from "remark-parse";
 import remarkStringify from "remark-stringify";
 import { unified } from "unified";
+import { select as unistUtilSelect } from "unist-util-select";
+import * as yaml from "yaml";
 
 const fileProcessor = unified()
   .use(remarkParse)
@@ -33,21 +44,108 @@ interface PageProps {
   params: PageParams;
 }
 
+interface YamlNode extends UnistNode {
+  value: string;
+}
+
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata> {
+  const file = await fs.promises.readFile(
+    path.resolve(contentDir, `${params.id}.md`)
+  );
+  const tree = await fileProcessor.run(fileProcessor.parse(file.toString()));
+  let title = "";
+  let description = "";
+  let publishedTime: string | undefined;
+  let tags: string[] | undefined;
+  const yamlNode = unistUtilSelect("yaml", tree) as YamlNode | null;
+  if (yamlNode) {
+    const yamlData = yaml.parse(yamlNode.value);
+    if (yamlData.title) title = yamlData.title;
+    if (yamlData.description) description = yamlData.description;
+    if (yamlData.date) publishedTime = yamlData.date;
+    if (yamlData.tags) tags = yamlData.tags;
+  }
+  if (!title) title = mdastToString(unistUtilSelect("heading[depth=1]", tree));
+  if (!description)
+    description = mdastToString(unistUtilSelect("paragraph", tree));
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      type: "article",
+      authors: ["https://twitter.com/phuctm97"],
+      tags,
+      publishedTime,
+    },
+  };
+}
+
 async function getPageContent(params: PageParams): Promise<string> {
   const file = await fs.promises.readFile(
     path.resolve(contentDir, `${params.id}.md`)
   );
-  const fileTree = await fileProcessor.run(
-    fileProcessor.parse(file.toString())
-  );
-  const contentChildren = fileTree.children.filter((n) => n.type !== "yaml");
-  const contentTree: Root = { type: "root", children: contentChildren };
-  return contentProcessor.stringify(contentTree);
+  const tree = await fileProcessor.run(fileProcessor.parse(file.toString()));
+  const contentChildren = tree.children.filter((n) => n.type !== "yaml");
+  const contentRoot: MdastRoot = { type: "root", children: contentChildren };
+  return contentProcessor.stringify(contentRoot);
 }
+
+const reactMarkdownRemarkPlugins = [remarkGfm];
+const reactMarkdownRehypePlugins = [rehypeRaw];
+
+type NavLinkProps = Omit<
+  ComponentPropsWithoutRef<typeof Link>,
+  "target" | "rel"
+>;
+
+const NavLink = forwardRef<HTMLAnchorElement, NavLinkProps>(
+  ({ className, href, ...props }, ref) => {
+    const isExternal = typeof href === "string" && !href.startsWith("/");
+    return (
+      <Link
+        ref={ref}
+        className={clsx(
+          "truncate text-gray-600 hover:text-gray-900 hover:underline",
+          className
+        )}
+        href={href}
+        target={isExternal ? "_blank" : undefined}
+        rel={isExternal ? "noopener noreferrer" : undefined}
+        {...props}
+      />
+    );
+  }
+);
+
+if (process.env.NODE_ENV === "development") NavLink.displayName = "NavLink";
 
 const Page: SC<PageProps> = async ({ params }) => {
   const content = await getPageContent(params);
-  return <ReactMarkdown>{content}</ReactMarkdown>;
+  return (
+    <>
+      <header className="mx-auto mb-5 mt-10 w-full max-w-2xl overflow-hidden px-5 md:mt-12 lg:mt-14">
+        <nav className="flex w-full flex-row items-center justify-start space-x-5 overflow-hidden">
+          <NavLink href="/">Home</NavLink>
+          <NavLink href="https://twitter.com/phuctm97">Twitter</NavLink>
+          <NavLink href="https://github.com/phuctm97">Github</NavLink>
+        </nav>
+      </header>
+      <main className="mx-auto mb-10 w-full max-w-2xl overflow-hidden px-5 md:mb-12 lg:mb-14">
+        <article className="prose max-w-none">
+          <ReactMarkdown
+            remarkPlugins={reactMarkdownRemarkPlugins}
+            rehypePlugins={reactMarkdownRehypePlugins}
+          >
+            {content}
+          </ReactMarkdown>
+        </article>
+      </main>
+    </>
+  );
 };
 
 export default Page;
--- apps/www/app/page.tsx
+++ apps/www/app/page.tsx
@@ -4,83 +4,64 @@ import { clsx } from "clsx";
 import Link from "next/link";
 import { forwardRef } from "react";
 
-import { Scroll } from "./scroll";
-
-const DefaultLink = forwardRef<
-  HTMLAnchorElement,
-  ComponentPropsWithoutRef<typeof Link>
->(({ className, ...props }, ref) => (
-  <Link
-    ref={ref}
-    className={clsx(
-      "text-blue-600 underline-offset-2 hover:text-blue-700 hover:underline",
-      className
-    )}
-    {...props}
-  />
-));
-
-if (process.env.NODE_ENV === "development")
-  DefaultLink.displayName = "DefaultLink";
-
-type ExternalLinkProps = Omit<
-  ComponentPropsWithoutRef<typeof DefaultLink>,
+type ContentLinkProps = Omit<
+  ComponentPropsWithoutRef<typeof Link>,
   "target" | "rel"
 >;
 
-const ExternalLink = forwardRef<HTMLAnchorElement, ExternalLinkProps>(
-  (props, ref) => (
-    <DefaultLink
-      ref={ref}
-      target="_blank"
-      rel="noopener noreferrer"
-      {...props}
-    />
-  )
+const ContentLink = forwardRef<HTMLAnchorElement, ContentLinkProps>(
+  ({ className, href, ...props }, ref) => {
+    const isExternal = typeof href === "string" && !href.startsWith("/");
+    return (
+      <Link
+        ref={ref}
+        className={clsx(
+          "text-blue-600 underline-offset-2 hover:text-blue-700 hover:underline",
+          className
+        )}
+        href={href}
+        target={isExternal ? "_blank" : undefined}
+        rel={isExternal ? "noopener noreferrer" : undefined}
+        {...props}
+      />
+    );
+  }
 );
 
 if (process.env.NODE_ENV === "development")
-  ExternalLink.displayName = "ExternalLink";
+  ContentLink.displayName = "ContentLink";
 
 const Page: FC = () => (
-  <Scroll>
-    <main className="mx-auto w-full max-w-2xl overflow-hidden px-5 py-10 md:py-12 lg:py-14">
-      <p className="mb-5">Hi, I&apos;m Phuc!</p>
-      <p className="mb-10">
-        I&apos;m a full-time indie hacker. I build Internet products to solve
-        problems for myself and others. I then charge a small fee to keep the
-        lights on and live independently.
-      </p>
-      <h2 className="mb-5 font-semibold">Posts</h2>
-      <p className="mb-10 text-gray-600">(No recent posts)</p>
-      <h2 className="mb-5 font-semibold">Tweets</h2>
-      <p className="mb-5">
-        I build in public on Twitter. I tweet product updates and learnings{" "}
-        <span className="italic">almost</span> every day, life updates
-        sometimes, and epic shit-posts most often.
-      </p>
-      <p className="mb-10">
-        See my tweets{" "}
-        <ExternalLink href="https://twitter.com/phuctm97">
-          @phuctm97
-        </ExternalLink>
-        .
-      </p>
-      <h2 className="mb-5 font-semibold">Sources</h2>
-      <p className="mb-5">
-        I open-source most of my recent work. It&apos;s building in public at
-        its finest. Besides, it helps me get over the idea that my code is
-        shitty, which it always is, to just ship it.
-      </p>
-      <p>
-        See my sources{" "}
-        <ExternalLink href="https://github.com/phuctm97">
-          @phuctm97
-        </ExternalLink>
-        .
-      </p>
-    </main>
-  </Scroll>
+  <main className="mx-auto w-full max-w-2xl overflow-hidden px-5 py-10 md:py-12 lg:py-14">
+    <p className="mb-5">Hi, I&apos;m Phuc!</p>
+    <p className="mb-10">
+      I&apos;m a full-time indie hacker. I build Internet products to solve
+      problems for myself and others. I then charge a small fee to keep the
+      lights on and live independently.
+    </p>
+    <h2 className="mb-5 font-semibold">Posts</h2>
+    <p className="mb-10 text-gray-600">(No recent posts)</p>
+    <h2 className="mb-5 font-semibold">Tweets</h2>
+    <p className="mb-5">
+      I build in public on Twitter. I tweet product updates and learnings{" "}
+      <span className="italic">almost</span> every day, life updates sometimes,
+      and epic shit-posts most often.
+    </p>
+    <p className="mb-10">
+      See my tweets{" "}
+      <ContentLink href="https://twitter.com/phuctm97">@phuctm97</ContentLink>.
+    </p>
+    <h2 className="mb-5 font-semibold">Sources</h2>
+    <p className="mb-5">
+      I open-source most of my recent work. It&apos;s building in public at its
+      finest. Besides, it helps me get over the idea that my code is shitty,
+      which it always is, to just ship it.
+    </p>
+    <p>
+      See my sources{" "}
+      <ContentLink href="https://github.com/phuctm97">@phuctm97</ContentLink>.
+    </p>
+  </main>
 );
 
 export default Page;
