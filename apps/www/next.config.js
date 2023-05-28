d2872c72549d8d60dc800042572fbb96a09b259c
phuctm97 <25026967+phuctm97@users.noreply.github.com>
2023-05-28 15:29:00 +0700

    Rewrite blog.phuctm97.com/* to phuctm97.com/blog/*

--- apps/www/next.config.js
+++ apps/www/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const config = {
   eslint: { ignoreDuringBuilds: true },
+  rewrites: () => ({
+    beforeFiles: [
+      {
+        has: [{ type: "host", value: "blog.phuctm97.com" }],
+        source: "/:path*",
+        destination: "/blog/:path*",
+      },
+    ],
+  }),
 };
 
 module.exports = config;
