6303d0f74fa4783f15e8b0878a62c6f370432223
phuctm97 <25026967+phuctm97@users.noreply.github.com>
2023-05-28 11:34:08 +0700

    Add metadata tags

--- apps/www/app/blog/[id]/page.tsx
+++ apps/www/app/blog/[id]/page.tsx
@@ -5,12 +5,17 @@ import type { SC } from "~/server";
 
 import { S3 } from "@aws-sdk/client-s3";
 import { clsx } from "clsx";
+import { select as hastSelect } from "hast-util-select";
+import { toText as hastToText } from "hast-util-to-text";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import { forwardRef } from "react";
 import ReactMarkdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
 import remarkGfm from "remark-gfm";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import { unified } from "unified";
 
 const s3 = new S3({
   region: process.env.AWS_DEFAULT_REGION,
@@ -40,9 +45,35 @@ export async function generateStaticParams(): Promise<PageParams[]> {
   return Array.from(ids).map<PageParams>((id) => ({ id }));
 }
 
+interface PageContent {
+  body: string;
+  tags: string[];
+  date?: Date;
+}
+
+async function generateContent(params: PageParams): Promise<PageContent> {
+  const object = await s3.getObject({
+    Bucket: process.env.S3_ARCHIVE_BUCKET_NAME,
+    Key: `blog/${params.id}`,
+  });
+  if (!object.Body) notFound();
+  const content: PageContent = {
+    body: await object.Body.transformToString(),
+    tags: object.Metadata?.tags ? object.Metadata.tags.split(",") : [],
+    date: object.Metadata?.date ? new Date(object.Metadata.date) : undefined,
+  };
+  return content;
+}
+
 interface PageProps {
   params: PageParams;
 }
+
+const contentProcessor = unified()
+  .use(remarkParse)
+  .use(remarkGfm)
+  .use(remarkRehype)
+  .use(rehypeRaw);
 
 export async function generateMetadata({
   params,
@@ -51,27 +82,39 @@ export async function generateMetadata({
     Bucket: process.env.S3_ARCHIVE_BUCKET_NAME,
     Key: `blog/${params.id}`,
   });
+  let title = object.Metadata?.title;
+  let description = object.Metadata?.description;
+  if (!title || !description) {
+    if (!object.Body) notFound();
+    const content = await object.Body.transformToString();
+    const tree = await contentProcessor.run(contentProcessor.parse(content));
+    if (!title) {
+      const h1 = hastSelect("h1", tree);
+      if (h1) title = hastToText(h1);
+    }
+    if (!description) {
+      const p = hastSelect("p", tree);
+      if (p) description = hastToText(p);
+    }
+  }
   return {
-    title: object.Metadata?.title,
-    description: object.Metadata?.description,
+    title,
+    description,
     openGraph: {
+      url: `blog/${params.id}`,
+      title,
+      description,
+      siteName: "Minh-Phuc Tran",
+      locale: "en_US",
       type: "article",
-      title: object.Metadata?.title,
-      description: object.Metadata?.description,
       publishedTime: object.Metadata?.date,
       tags: object.Metadata?.tags?.split(","),
-      authors: ["https://twitter.com/phuctm97"],
+      authors: [new URL("https://phuctm97.com")],
+    },
+    alternates: {
+      canonical: `blog/${params.id}`,
     },
   };
-}
-
-async function generateContent(params: PageParams): Promise<string> {
-  const object = await s3.getObject({
-    Bucket: process.env.S3_ARCHIVE_BUCKET_NAME,
-    Key: `blog/${params.id}`,
-  });
-  if (!object.Body) notFound();
-  return object.Body.transformToString();
 }
 
 const reactMarkdownRemarkPlugins = [remarkGfm];
@@ -120,8 +163,33 @@ const Page: SC<PageProps> = async ({ params }) => {
             remarkPlugins={reactMarkdownRemarkPlugins}
             rehypePlugins={reactMarkdownRehypePlugins}
           >
-            {content}
+            {content.body}
           </ReactMarkdown>
+          <hr />
+          <footer>
+            {content.tags.length > 0 && (
+              <ul className="pl-0">
+                Tags:
+                {content.tags.map((tag) => (
+                  <li key={tag} className="inline">
+                    {tag}
+                  </li>
+                ))}
+              </ul>
+            )}
+            {content.date && (
+              <p>
+                Date:{" "}
+                <time dateTime={content.date.toISOString()}>
+                  {content.date.toLocaleDateString("en", {
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                  })}
+                </time>
+              </p>
+            )}
+          </footer>
         </article>
       </main>
     </>
--- apps/www/app/layout.tsx
+++ apps/www/app/layout.tsx
@@ -8,11 +8,58 @@ import { Inconsolata, Inter } from "next/font/google";
 import { Configuration } from "./configuration";
 import { Shell } from "./shell";
 
-export const metadata: Metadata = {
-  metadataBase: new URL("https://phuctm97.com"),
-  title: "Minh-Phuc Tran",
+const info = {
+  url: new URL("https://phuctm97.com"),
+  title: { default: "Minh-Phuc Tran", template: "%s | Minh-Phuc Tran" },
   description:
     "I'm a full-time indie hacker. I build Internet products to solve problems for myself and others. I then charge a small fee to keep the lights on and live independently.",
+};
+
+export const metadata: Metadata = {
+  metadataBase: info.url,
+  title: info.title,
+  description: info.description,
+  applicationName: "Minh-Phuc Tran",
+  authors: [{ url: info.url, name: "Minh-Phuc Tran" }],
+  keywords: [
+    "phuctm97",
+    "Minh-Phuc Tran",
+    "phuctm",
+    "Phuc Tran",
+    "Minh-Phuc",
+    "Phuc",
+  ],
+  creator: "Minh-Phuc Tran",
+  publisher: "Minh-Phuc Tran",
+  generator: "Next.js",
+  openGraph: {
+    url: "/",
+    title: info.title,
+    description: info.description,
+    siteName: "Minh-Phuc Tran",
+    locale: "en_US",
+    type: "profile",
+    username: "phuctm97",
+    firstName: "Phuc",
+    lastName: "Tran",
+    gender: "male",
+  },
+  twitter: {
+    card: "summary",
+    site: "@phuctm97",
+    creator: "@phuctm97",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+  alternates: {
+    canonical: "/",
+  },
 };
 
 const inconsolata = Inconsolata({
