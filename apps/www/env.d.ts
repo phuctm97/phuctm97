00fccfd21fd33475db72b2db88c7446bac07d40f
phuctm97 <25026967+phuctm97@users.noreply.github.com>
2023-05-28 14:48:40 +0700

    Change env vars

--- apps/www/env.d.ts
+++ apps/www/env.d.ts
@@ -1,8 +1,8 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-    AWS_DEFAULT_REGION: string;
-    AWS_ACCESS_KEY_ID: string;
-    AWS_SECRET_ACCESS_KEY: string;
+    APP_AWS_REGION: string;
+    APP_AWS_ACCESS_KEY_ID: string;
+    APP_AWS_SECRET_ACCESS_KEY: string;
     S3_ARCHIVE_BUCKET_NAME: string;
   }
 }
--- apps/www/server/aws.ts
+++ apps/www/server/aws.ts
@@ -1,9 +1,9 @@
 import { S3 } from "@aws-sdk/client-s3";
 
 export const s3 = new S3({
-  region: process.env.AWS_DEFAULT_REGION,
+  region: process.env.APP_AWS_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.APP_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.APP_AWS_SECRET_ACCESS_KEY,
   },
 });
