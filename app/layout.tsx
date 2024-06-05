bd893ba492f214997281f19ebe6b8e837508f3a9
phuctm97 <phuctm97@gmail.com>
2024-06-05 22:34:00 +0800

    Add license & metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import type { PropsWithChildren, ReactNode } from "react";
 
 import localFont from "next/font/local";
@@ -15,6 +16,19 @@ const font = localFont({
     },
   ],
 });
+
+export const metadata: Metadata = {
+  title: "Minh-Phuc Tran - A portfolio with Windows 95 UI and ChatGPT-like AI",
+  description:
+    "I'm Minh-Phuc Tran, an indie hacker. I built this portfolio to bring together retro aesthetics (Windows 95) and futuristic technologies (ChatGPT).",
+  alternates: {
+    canonical: process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
+      ? new URL(
+          `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`,
+        )
+      : undefined,
+  },
+};
 
 export default function Layout({ children }: PropsWithChildren): ReactNode {
   return (
