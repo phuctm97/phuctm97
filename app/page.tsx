5c10a4cc7b172db31c60a8d5592f3dbe620b9783
phuctm97 <phuctm97@gmail.com>
2024-06-05 18:11:13 +0800

    Fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,44 @@
 
 import type { ReactNode } from "react";
 
+import { useAtomValue } from "jotai";
 import styled from "styled-components";
 
+import { ChatGPT } from "~/lib/chatgpt";
+import { Notepad } from "~/lib/notepad";
+import { Welcome } from "~/lib/welcome";
+import { openWindowsAtom } from "~/lib/window";
+
 import { Header } from "./header";
-import { Windows } from "./windows";
+
+interface WindowProps {
+  window: string;
+}
+
+function Window({ window }: WindowProps): ReactNode {
+  switch (window) {
+    case "Welcome": {
+      return <Welcome />;
+    }
+    case "Notepad": {
+      return <Notepad />;
+    }
+    case "ChatGPT": {
+      return <ChatGPT />;
+    }
+  }
+}
+
+function Windows(): ReactNode {
+  const openWindows = useAtomValue(openWindowsAtom);
+  return (
+    <>
+      {openWindows.map((window) => (
+        <Window key={window} window={window} />
+      ))}
+    </>
+  );
+}
 
 const Main = styled.main`
   position: relative;
