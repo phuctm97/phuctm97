4a6b9113440ea59f97830dbb484781ddee9dd2ea
phuctm97 <phuctm97@gmail.com>
2024-06-05 19:21:12 +0800

    Fix

--- lib/notepad.tsx
+++ lib/notepad.tsx
@@ -1,23 +1,46 @@
-import type { ReactNode } from "react";
+import type { ChangeEventHandler, ReactNode } from "react";
 
+import { atom, useAtom } from "jotai";
+import { useCallback } from "react";
 import { TextInput } from "react95";
 import styled from "styled-components";
 
 import { Window } from "~/lib/window";
-
-const StyledWindow = styled(Window)`
-  padding: 6px;
-`;
 
 const StyledTextInput = styled(TextInput)`
   flex-grow: 1;
   flex-shrink: 1;
 `;
 
+const valueAtom = atom("");
+
+function Input(): ReactNode {
+  const [value, setValue] = useAtom(valueAtom);
+  const handleChange = useCallback<ChangeEventHandler<HTMLTextAreaElement>>(
+    (event) => {
+      setValue(event.target.value);
+    },
+    [setValue],
+  );
+  return (
+    <StyledTextInput
+      value={value}
+      onChange={handleChange}
+      placeholder="Write something…"
+      spellCheck={false}
+      multiline
+    />
+  );
+}
+
+const StyledWindow = styled(Window)`
+  padding: 6px;
+`;
+
 export function Notepad(): ReactNode {
   return (
     <StyledWindow window="Notepad" defaultWidth={400} defaultHeight={400}>
-      <StyledTextInput spellCheck={false} multiline />
+      <Input />
     </StyledWindow>
   );
 }
