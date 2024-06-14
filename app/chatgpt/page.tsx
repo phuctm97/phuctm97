adcc9cc2f4aca4651ba0a5f9254753c0e325cf6d
phuctm97 <phuctm97@gmail.com>
2024-06-14 18:53:44 +0800

    Add /chatgpt, /notepad

---
+++ app/chatgpt/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import type { ReactNode } from "react";
+
+import { Windows } from "~/lib/window";
+
+export default function Page(): ReactNode {
+  return <Windows windows={["ChatGPT"]} />;
+}
