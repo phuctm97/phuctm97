f607fe9d88e7323b750d051b620e504ecfb7dd0c
Duy Bui <khanhduyvt0101@gmail.com>
2024-08-22 15:56:07 +0700

    fix: update current url

--- lib/config.ts
+++ lib/config.ts
@@ -1,14 +1,11 @@
-export function getCurrentOrigin(): string {
-  switch (process.env.NEXT_PUBLIC_ENV) {
-    case "production": {
-      return "https://www.phuctm97.com";
-    }
-    default: {
-      return "http://localhost:43815";
-    }
-  }
-}
-
 export function getCurrentURL(path = "/"): URL {
-  return new URL(path, getCurrentOrigin());
+  return new URL(
+    path,
+    process.env.NEXT_PUBLIC_VERCEL_ENV === "production" &&
+    process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
+      : process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL
+        ? `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`
+        : "http://localhost:43815",
+  );
 }
